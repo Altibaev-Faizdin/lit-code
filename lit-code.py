a9eaf9b2ffb5a30ad53a55d9f52970962200c33f
Altibaev-Faizdin <faizaltibaev@gmail.com>
2026-07-21 23:04:14 +0600

    lit code - day 20

--- lit-code.py
+++ lit-code.py
@@ -683,3 +683,40 @@
 # def count_smileys(arr):
 #     return sum(1 for i in arr if i in [':)', ':D', ';)', ';D', ':-)', ':-D', ';-)', ';-D', ':~)', ':~D', ';~)', ';~D'])
 
+
+
+
+
+
+#47. Mumbling
+
+# def likes(names):
+#     if len(names) == 0:
+#         return "no one likes this"
+#     elif len(names) == 1:
+#         return f"{names[0]} likes this"
+#     elif len(names) == 2:
+#         return f"{names[0]} and {names[1]} like this"
+#     elif len(names) == 3:
+#         return f"{names[0]}, {names[1]} and {names[2]} like this"
+#     else:
+#         return f"{names[0]}, {names[1]} and {len(names) - 2} others like this"
+
+# print(likes([]))
+# print(likes(["Peter"]))
+# print(likes(["Jacob", "Alex"]))
+# print(likes(["Max", "John", "Mark"]))
+# print(likes(["Alex", "Jacob", "Mark", "Max"]))
+
+
+
+
+
+
+#48.
+
+def count_by(s):
+    result = {}
+    for char in s:
+        result[char] = result.get(char, 0) + 1
+    return result
