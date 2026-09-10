e0cae47cfced3146053b65bdd27a9b47c9b30e5e
Altibaev-Faizdin <faizaltibaev@gmail.com>
2026-09-10 20:51:36 +0600

    lit code - day 45

--- lit-code.py
+++ lit-code.py
@@ -1732,3 +1732,23 @@
 
 # def digitize(n):
 #     return [int(d) for d in str(n)][::-1]
+
+
+
+
+#103. Maximum subarray sum
+
+# def max_sequence(arr):
+#     max_sum = 0
+#     current_sum = 0
+#     for num in arr:
+#         current_sum = max(0, current_sum + num)
+#         max_sum = max(max_sum, current_sum)
+#     return max_sum
+
+
+
+#104. Convert a string to an array
+
+# def string_to_array(s):
+#     return s.split(" ")
